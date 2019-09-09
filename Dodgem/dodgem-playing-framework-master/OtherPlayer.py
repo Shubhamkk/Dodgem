c40fb120edd13612fd3fff2dc224a000fbb51b28
Shubhamkk <shubhamssss752@gmail.com>
2019-09-09 18:12:02 +0530

    Update OtherPlayer.py

--- Dodgem/dodgem-playing-framework-master/OtherPlayer.py
+++ Dodgem/dodgem-playing-framework-master/OtherPlayer.py
@@ -1,11 +1,4 @@
-'''
-    File name: OtherPlayer.py
-    Author: Siddhant Kumar
-    Email: saytosid@gmail.com
-    Date created: 1 Oct 2017
-    Date last modified: 1 Oct 2017
-    Python Version: 3.0
-'''
+
 from framework import Player
 
 class OtherPlayer(Player):
